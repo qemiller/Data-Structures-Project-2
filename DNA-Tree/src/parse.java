e3c956847da404a054ddcac5afea2a9fcd88ae54
Quinton <qemiller@vt.edu>
2019-03-05 14:48:05 -0500

    added some trie calls in the parser

--- DNA-Tree/src/parse.java
+++ DNA-Tree/src/parse.java
@@ -3,27 +3,30 @@ import java.io.File;
 import java.util.Scanner;
 
 public class parse {
-    public void parseFile(String fileName)
-    {
-        try
-        {
-            Scanner sc = new Scanner (new File(fileName));
-            while(sc.hasNextLine())
-            {
+    public void parseFile(String fileName) {
+        try {
+            Scanner sc = new Scanner(new File(fileName));
+            Trie tree = new Trie();
+            while (sc.hasNextLine()) {
                 String command = sc.next();
                 String sequence;
-                switch(command)
-                {
+                switch (command) {
+                    
                     case "insert":
                         sequence = sc.next();
-                        
+                        tree.insert(sequence);
+                        break;
+
+                    case "remove":
+                        sequence = sc.next();
+                        tree.remove(sequence);
                         break;
                 }
             }
+            sc.close();
         }
-        catch(Exception FileNotFoundException)
-        {
-            //Throw this if you can't find the file.
+        catch (Exception FileNotFoundException) {
+            // Throw this if you can't find the file.
         }
     }
 }
